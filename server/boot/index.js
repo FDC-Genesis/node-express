f8c1baf4161cf34b96e5c3cca7b36a0e0ac10e0d
kyuuukira <genesistroy.fdc@gmail.com>
2024-11-11 10:58:52 +0800

    revert

--- server/boot/index.js
+++ server/boot/index.js
@@ -18,152 +18,155 @@ const csrf = require('../../libs/Middleware/Csrf');
 const PathFinder = require('../../libs/Service/PathFinder');
 Boot.up();
 
-class Application {
-    constructor() {
-        this.app = express();
-        this.setup();
-        this.app.use(cookieParser());
-        this.app.use(express.json());
-        this.app.use(express.urlencoded({ extended: true }));
-        this.app.use(express.static(PathFinder.public_path()));
-        this.app.use(csrf);
-        this.app.set('views', PathFinder.view_path());
-        this.app.set('view engine', 'ejs');
-    }
-    setup() {
-        let store = null;
+const app = express();
 
-        if (process.env.NODE_ENV === 'production') {
-            store = Configure.read(`session.${process.env.SESSION_STORE}`)();
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(PathFinder.public_path()));
+
+
+
+let store = null;
+
+if (process.env.NODE_ENV === 'production') {
+    store = Configure.read(`session.${process.env.SESSION_STORE}`)();
+} else {
+    // store = new FileStore({
+    //     path: './database/sessions',
+    // });
+    store = new SQLiteStore({
+        db: 'sessions.sqlite',
+        dir: 'database',
+        table: 'sessions',
+        ttl: 86400,
+        concurrentDB: true
+    });
+    // store = new session.MemoryStore();
+}
+
+const sessionObj = {
+    secret: process.env.MAIN_KEY || 'test-secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: false,
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    },
+};
+
+if (store) {
+    sessionObj.store = store;
+}
+app.use(session(sessionObj));
+app.use(flash());
+app.use(csrf);
+app.use((req, res, next) => {
+    if (!req.session) {
+        req.session = {};
+    }
+    if (!req.session['auth']) {
+        req.session['auth'] = {};
+        req.session['auth'] = Configure.read('app');
+    }
+
+    if (!req.cookies['auth']) {
+        res.cookie('auth', JSON.stringify(Configure.read('app')), {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24 * 7,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'Lax',
+        });
+    }
+
+
+
+    if (!req.session['global_variables']) {
+        req.session['global_variables'] = {};
+    }
+
+    next();
+});
+app.set('views', PathFinder.view_path());
+app.set('view engine', 'ejs');
+
+// Route middleware to check and setup route variables
+app.use((req, res, next) => {
+    res.locals.config = (value) => Configure.read(value);
+    if (typeof res.auth !== 'undefined' || typeof res.auth !== 'function') {
+        res.auth = () => new Auth(req, res);
+    }
+    res.locals.auth = () => res.auth();
+    req.uriPath = req.path.split('/');
+    req.uriPath.shift();
+
+    const routeSource = Object.keys(guards);
+    const filteredRouteSource = routeSource.filter(route => route !== defaultGuard);
+    if (!filteredRouteSource.includes(req.uriPath[0])) req.uriPath.unshift(defaultGuard);
+
+    const [type, controller] = req.uriPath;
+    req.routeSrc = { type, controller };
+
+    next();
+});
+
+// Route rendering logic
+app.use((req, res, next) => {
+    const originalRender = res.render;
+
+    res.render = (view, locals, callback) => {
+        const viewPath = PathFinder.view_path();
+        let newView;
+        if (view !== 'Error') {
+            newView = `${ucFirst(req.routeSrc.type)}/${ucFirst(req.routeSrc.controller || defaultController)}/${view}`;
+            if (fs.existsSync(path.join(`${viewPath}${newView}.ejs`))) {
+                res.status(200);
+            } else {
+                locals = { message: 'Page Not Found', home: req.routeSrc.type };
+                newView = 'Error';
+                res.status(404);
+            }
         } else {
-            // store = new FileStore({
-            //     path: './database/sessions',
-            // });
-            store = new SQLiteStore({
-                db: 'sessions.sqlite',
-                dir: 'database',
-                table: 'sessions',
-                ttl: 86400,
-                concurrentDB: true
-            });
-            // store = new session.MemoryStore();
+            if (locals.home === undefined) locals.home = req.routeSrc.type;
+            newView = 'Error';
+            res.status(404);
         }
-
-        const sessionObj = {
-            secret: process.env.MAIN_KEY || 'test-secret',
-            resave: false,
-            saveUninitialized: true,
-            cookie: {
-                secure: false,
-                httpOnly: true,
-                maxAge: 1000 * 60 * 60 * 24 * 7
-            },
-        };
-
-        if (store) {
-            sessionObj.store = store;
+        if (!res.headersSent) {
+            return originalRender.call(res, newView, locals, callback);
         }
-        this.app.use(session(sessionObj));
-        this.app.use(flash());
-        this.app.use((req, res, next) => {
-            if (!req.session) {
-                req.session = {};
-            }
-            if (!req.session['auth']) {
-                req.session['auth'] = {};
-                req.session['auth'] = Configure.read('app');
-            }
+    };
 
-            if (req.cookies === undefined || !req.cookies['auth']) {
-                res.cookie('auth', JSON.stringify(Configure.read('app')), {
-                    httpOnly: true,
-                    maxAge: 1000 * 60 * 60 * 24 * 7,
-                    secure: process.env.NODE_ENV === 'production',
-                    sameSite: 'Lax',
-                });
-            }
+    next();
+});
+
+// Paginator middleware
+app.use((req, res, next) => {
+    res.paginator = () => new Paginator(req);
+    next();
+});
 
 
+// Helper function to capitalize the first letter
+function ucFirst(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
-            if (!req.session['global_variables']) {
-                req.session['global_variables'] = {};
-            }
+// Routes can now be declared here as before
+const apiRoutes = require('./api-index');
+app.use('/api', apiRoutes);
 
-            next();
-        });
-
-        this.app.use((req, res, next) => {
-            res.locals.config = (value) => Configure.read(value);
-            if (typeof res.auth !== 'undefined' || typeof res.auth !== 'function') {
-                res.auth = () => new Auth(req, res);
-            }
-            res.locals.auth = () => res.auth();
-            req.uriPath = req.path.split('/');
-            req.uriPath.shift();
-
-            const routeSource = Object.keys(guards);
-            const filteredRouteSource = routeSource.filter(route => route !== defaultGuard);
-            if (!filteredRouteSource.includes(req.uriPath[0])) req.uriPath.unshift(defaultGuard);
-
-            const [type, controller] = req.uriPath;
-            req.routeSrc = { type, controller };
-
-            next();
-        });
-
-        this.app.use((req, res, next) => {
-            const originalRender = res.render;
-
-            res.render = (view, locals, callback) => {
-                const viewPath = PathFinder.view_path();
-                let newView;
-                if (view !== 'Error') {
-                    newView = `${this.ucFirst(req.routeSrc.type)}/${this.ucFirst(req.routeSrc.controller || defaultController)}/${view}`;
-                    if (fs.existsSync(path.join(`${viewPath}${newView}.ejs`))) {
-                        res.status(200);
-                    } else {
-                        locals = { message: 'Page Not Found', home: req.routeSrc.type };
-                        newView = 'Error';
-                        res.status(404);
-                    }
-                } else {
-                    if (locals.home === undefined) locals.home = req.routeSrc.type;
-                    newView = 'Error';
-                    res.status(404);
-                }
-                if (!res.headersSent) {
-                    return originalRender.call(res, newView, locals, callback);
-                }
-            };
-
-            next();
-        });
-
-        // Paginator middleware
-        this.app.use((req, res, next) => {
-            res.paginator = () => new Paginator(req);
-            next();
-        });
-
-        const apiRoutes = require('./api-index');
-        this.app.use('/api', apiRoutes);
-
-        const guardsKeys = Object.keys(guards);
-        const authConfig = Configure.read('auth');
-        const appBaseRoute = PathFinder.app_path();
-        for (const ele of guardsKeys) {
-            const provider = guards[ele].provider;
-            const entityPrefix = authConfig.providers[provider].prefix;
-            const directory = `${appBaseRoute}${authConfig.providers[provider].entity}/Route`;
-            if (fs.existsSync(`${directory}/index.js`)) {
-                this.app.use(entityPrefix, require(directory));
-            }
-        }
-    }
-
-    ucFirst(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
+const guardsKeys = Object.keys(guards);
+const authConfig = Configure.read('auth');
+const appBaseRoute = PathFinder.app_path();
+for (const ele of guardsKeys) {
+    const provider = guards[ele].provider;
+    const entityPrefix = authConfig.providers[provider].prefix;
+    const directory = `${appBaseRoute}${authConfig.providers[provider].entity}/Route`;
+    if (fs.existsSync(`${directory}/index.js`)) {
+        app.use(entityPrefix, require(directory));
     }
 }
 
-module.exports = new Application().app;
+
+module.exports = app;
